235f9a03a248333c1ceb5b6cf7fa0b4f10172ecd
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-25 18:45:51 +0000

    Fix spelling mistake

--- src/main/java/org/learnwithllew/duplicationkata/Lesson21.java
+++ src/main/java/org/learnwithllew/duplicationkata/Lesson21.java
@@ -11,7 +11,7 @@ public class Lesson21 extends Song
         {
           if (name.startsWith("L", 0))
           {
-            sing("Hip Hip Horray! For " + name);
+            sing("Hip Hip Hooray! For " + name);
           }
           else
           {
