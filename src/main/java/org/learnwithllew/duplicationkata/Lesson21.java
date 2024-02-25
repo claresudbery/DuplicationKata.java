11478560381e122d4df20be9968f09d3386459d8
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-25 19:52:30 +0000

    Fix indentation and make vertical param list

--- src/main/java/org/learnwithllew/duplicationkata/Lesson21.java
+++ src/main/java/org/learnwithllew/duplicationkata/Lesson21.java
@@ -7,36 +7,41 @@ public class Lesson21 extends Song
     switch (style)
     {
       case 1 : {
-          boolean checkNameStart = true;
-          String namePrefix = "L";
-          String specialGreeting = "Hip Hip Hurray! For ";
-          int startOffset = 0;
+        boolean checkNameStart = true;
+        String namePrefix = "L";
+        String specialGreeting = "Hip Hip Hurray! For ";
+        int startOffset = 0;
 
         singRefrain(names, checkNameStart, namePrefix, startOffset, specialGreeting);
       }
-        break;
+      break;
       case 2 : {
-          boolean checkNameStart = true;
-          String namePrefix = "am";
-          String specialGreeting = "Say yeah! Say yo! Say ";
-          int startOffset = 1;
+        boolean checkNameStart = true;
+        String namePrefix = "am";
+        String specialGreeting = "Say yeah! Say yo! Say ";
+        int startOffset = 1;
 
         singRefrain(names, checkNameStart, namePrefix, startOffset, specialGreeting);
       }
-        break;
+      break;
       case 3 : {
-          boolean checkNameStart = false;
-          String namePrefix = "L";
-          String specialGreeting = "Hip Hip Hurray! For ";
-          int startOffset = 0;
+        boolean checkNameStart = false;
+        String namePrefix = "L";
+        String specialGreeting = "Hip Hip Hurray! For ";
+        int startOffset = 0;
 
         singRefrain(names, checkNameStart, namePrefix, startOffset, specialGreeting);
       }
-        break;
+      break;
     }
   }
 
-  private void singRefrain(String[] names, boolean checkNameStart, String namePrefix, int startOffset, String specialGreeting) {
+  private void singRefrain(
+          String[] names,
+          boolean checkNameStart,
+          String namePrefix,
+          int startOffset,
+          String specialGreeting) {
     for (String name : names) {
       if (checkNameStart && name.startsWith(namePrefix, startOffset)) {
         sing(specialGreeting + name);
