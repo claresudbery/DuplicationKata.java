8d0076164e4967b487b3a1e6e4df609d30d22ba7
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-25 19:48:58 +0000

    Move var declarations to before for loops

--- src/main/java/org/learnwithllew/duplicationkata/Lesson21.java
+++ src/main/java/org/learnwithllew/duplicationkata/Lesson21.java
@@ -7,11 +7,12 @@ public class Lesson21 extends Song
     switch (style)
     {
       case 1 : {
+          boolean checkNameStart = true;
+          String namePrefix = "L";
+          String specialGreeting = "Hip Hip Hurray! For ";
+          int startOffset = 0;
+        
           for (String name : names) {
-            boolean checkNameStart = true;
-            String namePrefix = "L";
-            String specialGreeting = "Hip Hip Hurray! For ";
-            int startOffset = 0;
             if (checkNameStart && name.startsWith(namePrefix, startOffset)) {
               sing(specialGreeting + name);
             } else {
@@ -21,11 +22,12 @@ public class Lesson21 extends Song
         }
         break;
       case 2 : {
+          boolean checkNameStart = true;
+          String namePrefix = "am";
+          String specialGreeting = "Say yeah! Say yo! Say ";
+          int startOffset = 1;
+
           for (String name : names) {
-            boolean checkNameStart = true;
-            String namePrefix = "am";
-            String specialGreeting = "Say yeah! Say yo! Say ";
-            int startOffset = 1;
             if (checkNameStart && name.startsWith(namePrefix, startOffset)) {
               sing(specialGreeting + name);
             } else {
@@ -35,18 +37,19 @@ public class Lesson21 extends Song
         }
         break;
       case 3 : {
-            for (String name : names) {
-              boolean checkNameStart = false;
-              String namePrefix = "L";
-              String specialGreeting = "Hip Hip Hurray! For ";
-              int startOffset = 0;
-              if (checkNameStart && name.startsWith(namePrefix, startOffset)) {
-                sing(specialGreeting + name);
-              } else {
-                sing("Hello " + name + ", it's nice to meet you.");
-              }
+          boolean checkNameStart = false;
+          String namePrefix = "L";
+          String specialGreeting = "Hip Hip Hurray! For ";
+          int startOffset = 0;
+
+          for (String name : names) {
+            if (checkNameStart && name.startsWith(namePrefix, startOffset)) {
+              sing(specialGreeting + name);
+            } else {
+              sing("Hello " + name + ", it's nice to meet you.");
             }
           }
+        }
         break;
     }
   }
